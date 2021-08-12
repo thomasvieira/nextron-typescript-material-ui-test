525c46d6bb71939c52264ee7e1f3122f9db293d6
THOMAS <thomas@rocketlab.com.br>
2021-08-11 21:46:46 -0300

    feat: implement Tray menu, Tray icon and events for minimizing and maximizing

--- main/background.ts
+++ main/background.ts
@@ -1,9 +1,10 @@
-import { app } from "electron";
+import { app, Menu, Tray } from "electron";
 import serve from "electron-serve";
+import path from "path";
 import { createWindow } from "./helpers";
 
 const isProd: boolean = process.env.NODE_ENV === "production";
-
+let tray = null;
 if (isProd) {
   serve({ directory: "app" });
 } else {
@@ -11,7 +12,41 @@ if (isProd) {
 }
 
 (async () => {
-  await app.whenReady();
+  await app.whenReady().then(() => {
+    let iconPath = null;
+    if (isProd) {
+      iconPath = path.join(__dirname, "/resources/icon.ico");
+    } else {
+      iconPath = path.join(__dirname, "../resources/icon.ico");
+    }
+    tray = new Tray(iconPath);
+
+    const contextMenu = Menu.buildFromTemplate([
+      {
+        label: "Show",
+        click: () => {
+          mainWindow.setSkipTaskbar(false);
+          mainWindow.show();
+        },
+      },
+      { label: "Item2", type: "radio" },
+      { label: "Item3", type: "radio", checked: true },
+      {
+        label: "Quit",
+        role: "quit",
+      },
+    ]);
+    tray.setToolTip("BrainSpike App");
+    tray.setContextMenu(contextMenu);
+    tray.setIgnoreDoubleClickEvents(true);
+    tray.on("click", function (e) {
+      if (mainWindow.isVisible()) {
+        mainWindow.hide();
+      } else {
+        mainWindow.show();
+      }
+    });
+  });
 
   const mainWindow = createWindow("main", {
     width: 1000,
@@ -25,8 +60,23 @@ if (isProd) {
     await mainWindow.loadURL(`http://localhost:${port}/home`);
     //mainWindow.webContents.openDevTools();
   }
+
+  mainWindow.hide();
+
+  mainWindow.setSkipTaskbar(true);
+
+  mainWindow.on("restore", () => {
+    mainWindow.setSkipTaskbar(false);
+    mainWindow.show();
+  });
+
+  mainWindow.on("minimize", () => {
+    mainWindow.setSkipTaskbar(true);
+    mainWindow.hide();
+  });
 })();
 
 app.on("window-all-closed", () => {
+  tray.destroy();
   app.quit();
 });
